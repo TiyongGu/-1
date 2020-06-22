68bb7bd466e5911cbc925b47a945002d5212e9c9
ex-yuty001 <ex-yuty001@IQSZ-D0121.cmrh.com>
2020-06-22 17:06:30 +0800

    ID测试

--- dbutils/src/com/iotek/entity/Animals.java
+++ dbutils/src/com/iotek/entity/Animals.java
@@ -1,7 +1,9 @@
 package com.iotek.entity;
 
 public class Animals {
+	
      private Integer idesds;
+     private Integer 123213;
      private Integer id;
      private String name;
      private float weight;
